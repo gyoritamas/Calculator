cb20ba4a61e6fb4750caa2174828d0bd65ae9fce
gyoritamas <gyoritamasnoe@gmail.com>
2020-11-15 13:20:21 +0100

    trying to clean up code

--- Calculator/Form1.cs
+++ Calculator/Form1.cs
@@ -18,12 +18,22 @@ namespace Calculator
 
         }
 
-        public string Screen {
+        private string Screen
+        {
             get => textBoxResult.Text;
             set => textBoxResult.Text = value;
         }
+        private void RemoveLastChars(int numberOfChars)
+        {
+            Screen = Screen.Substring(0, Screen.Length - numberOfChars);
+        }
 
-        bool calculationDone = false;
+        private char LastChar()
+        {
+            return Screen[Screen.Length - 1];
+        }
+
+        bool resultOnScreen = false;
         double lastResult = 0;
         private void buttonNumber_Click(object sender, EventArgs e)
         {
@@ -33,17 +43,17 @@ namespace Calculator
             // the very first input is number:
             // overwrite 0 the program starts with
             // also reset the result of the previous calculation
-            if (Screen == "0" || calculationDone)
+            if (Screen == "0" || resultOnScreen)
             {
                 Screen = "";
-                calculationDone = false;
+                resultOnScreen = false;
             }
 
             // don't let the user write numbers starting with 0
             // unless they follow up with ","                
             if (Screen.EndsWith(" 0"))
-                Screen = Screen.Substring(0, Screen.Length - 1);
-            AppendNumber(button);
+                RemoveLastChars(1);
+            Append(button);
         }
 
         private void buttonOperator_Click(object sender, EventArgs e)
@@ -51,41 +61,37 @@ namespace Calculator
             Button clickedButton = sender as Button;
             string button = clickedButton.Text;
 
-            if (calculationDone)
+            if (resultOnScreen)
             {
                 Screen = lastResult.ToString();
-                calculationDone = false;
+                resultOnScreen = false;
             }
 
             // last input also was an operator
             if (Screen.EndsWith(" "))
-                OverwriteLastOperator(button);
-            // last input was a number
-            else
-                AppendOperator(button);
-        }
+                RemoveLastChars(3);
 
-        private string OverwriteLastOperator(string operation)
-        {
-            //clear last operator and whitespace
-            Screen = Screen.Substring(0, Screen.Length - 3);
-            return AppendOperator(operation);
+            Append(button);
+            
         }
+        
         //TODO: handle input from keys, not just mouse (Keypress event)
-        private string AppendOperator(string operation)
-        {
-            Screen = Screen.TrimEnd(',');
-            return Screen += " " + operation + " ";
-        }
 
-        private string AppendNumber(string number)
+        private void Append(string button)
         {
-            return Screen += number;
+            if (char.IsDigit(button[0]) || button.Equals(","))
+            {
+                Screen += button;
+            } else
+            {
+                Screen = Screen.TrimEnd(',');
+                Screen += " " + button + " ";
+            }
         }
-
+        
         private void buttonClear_Click(object sender, EventArgs e)
         {
-            if (calculationDone) return;
+            if (resultOnScreen) return;
             // if only a one digit number left (positive or negative) set the screen to 0
             if (Screen.Length == 1 || (Screen.Length == 2 && Screen.StartsWith("-")))
             {
@@ -96,20 +102,20 @@ namespace Calculator
             if (Screen.Length > 1)
             {
                 if (Screen.EndsWith(" "))
-                    Screen = Screen.Substring(0, Screen.Length - 3);
+                    RemoveLastChars(3);
                 else
                 {
-                    Screen = Screen.Substring(0, Screen.Length - 1);
+                    RemoveLastChars(1);
                 }
             }
         }
 
         private void buttonNegate_Click(object sender, EventArgs e)
         {
-            if (calculationDone)
+            if (resultOnScreen)
             {
                 Screen = lastResult.ToString();
-                calculationDone = false;
+                resultOnScreen = false;
             }
 
             if (Screen != "0" && !Screen.EndsWith(" "))
@@ -142,19 +148,15 @@ namespace Calculator
 
         private void buttonDecimal_Click(object sender, EventArgs e)
         {
-            if (calculationDone) return;
-            if (!Screen.EndsWith(" ") && !Screen.EndsWith("-"))
-            {
-                //this is the first number or the screen ends with a number not containing ","
-                if (!Screen.Contains(" ") || Screen.LastIndexOf(" ") > Screen.LastIndexOf(","))
-                    AppendNumber(",");
-            }
-
+            if (resultOnScreen) return;
+            if (char.IsDigit(LastChar()) && Screen.LastIndexOf(" ") >= Screen.LastIndexOf(","))
+                Append(",");
         }
 
         private void buttonCalculate_Click(object sender, EventArgs e)
         {
-            if (calculationDone) return;
+            if (resultOnScreen) return;
+            Screen = Screen.Trim(',');
             string[] calcString = Screen.Split(' ');
             double tempResult = 0;
             double nextDouble = 0;
@@ -192,12 +194,11 @@ namespace Calculator
                 }
             }
 
-            calculationDone = true;
+            resultOnScreen = true;
             lastResult = tempResult;
             Screen += Environment.NewLine + tempResult;
         }
 
-        //TODO: after calculate, at next click clean everything and put result in the first line
-        //TODO: if lastresult was NaN, set lastresult to 0 before calculating with it
+        //TODO: if lastresult was NaN of infinity, set lastresult to 0 before calculating with it
     }
 }
